cd10e02896549d7a908ff9ad3124057bea48cb3c
AndresGarcia96 <andresfgarciac96@gmail.com>
2025-09-28 10:27:56 -0500

    creation of project framework

---
+++ apps/api/src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // app.setGlobalPrefix('api-crosspay/1.0');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('CrossPay')
+    .setDescription('Mini-plataforma de pagos con portal administrativo')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(+process.env.API_PORT || 3001);
+}
+bootstrap();
